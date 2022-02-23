ab6f3aff5d21477c59032c02faa4280c97f989bc
KamiReo HR Jois <divineleo20@gmail.com>
2022-02-23 12:55:13 +0530

    finished Header

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './App.scss';
-import { About, Footer, Header, Skills, Testimonials, Work } from './containers'
-import { Navbar } from './components';
+import React from "react";
+import "./App.scss";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonials,
+  Work,
+} from "./containers";
+import { Navbar } from "./components";
 
 const App = () => {
-    return (
-        <div className='app'>
-            <Header />
-            <Navbar />
-            
-            <About />
-            <Work />
-            <Skills />
-            <Testimonials />
-            <Footer />
-        </div>
-    );
-}
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
